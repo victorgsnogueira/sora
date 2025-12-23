31ccc316967b6993dc9abb492ba1412e6f7b4a3d
Victor Nogueira <vgdsno@gmail.com>
2025-12-23 16:09:42 -0300

    chore: adicionar e configurar libs para desenvolvimento

--- sora-core/src/server.ts
+++ sora-core/src/server.ts
@@ -1,10 +1,41 @@
+import { fastifyCors } from '@fastify/cors';
+import { fastifySwagger } from '@fastify/swagger';
+import ScalarApiReference from '@scalar/fastify-api-reference';
 import { fastify } from 'fastify';
-import { env } from 'process';
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod';
 
-const port = env.PORT;
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-const app = fastify();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.listen({ port: Number(port), host: '0.0.0.0' }).then(() => {
-  console.log(`Server running on http://localhost:${port}`);
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Sora Core',
+      description:
+        'API REST construída com Fastify para gerenciamento de usuários, compradores e bots de ticket. Responsável por autenticação, configuração de mensagens personalizadas e integração com múltiplas instâncias de bots.',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+});
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log(`Server running on http://localhost:3333`);
+  console.log(`Docs avaliable at http://localhost:3333/docs`);
 });
