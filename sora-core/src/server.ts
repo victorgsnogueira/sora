6d1409223fc6df5bbb9d2faabbb0304978bf6f9a
Victor Nogueira <vgdsno@gmail.com>
2025-12-24 17:19:10 -0300

    feat: implementar a configuração inicial do ORM Prisma e configuração do Docker Compose.

--- sora-core/src/server.ts
+++ sora-core/src/server.ts
@@ -3,10 +3,10 @@ import { fastifySwagger } from '@fastify/swagger';
 import ScalarApiReference from '@scalar/fastify-api-reference';
 import { fastify } from 'fastify';
 import {
-  jsonSchemaTransform,
-  serializerCompiler,
-  validatorCompiler,
-  type ZodTypeProvider,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -15,27 +15,27 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(fastifyCors, {
-  origin: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+	origin: true,
+	methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 });
 
 app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: 'Sora Core',
-      description:
-        'API REST construída com Fastify para gerenciamento de usuários, compradores e bots de ticket. Responsável por autenticação, configuração de mensagens personalizadas e integração com múltiplas instâncias de bots.',
-      version: '1.0.0',
-    },
-  },
-  transform: jsonSchemaTransform,
+	openapi: {
+		info: {
+			title: 'Sora Core',
+			description:
+				'API REST construída com Fastify para gerenciamento de usuários, compradores e bots de ticket. Responsável por autenticação, configuração de mensagens personalizadas e integração com múltiplas instâncias de bots.',
+			version: '1.0.0',
+		},
+	},
+	transform: jsonSchemaTransform,
 });
 
 app.register(ScalarApiReference, {
-  routePrefix: '/docs',
+	routePrefix: '/docs',
 });
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log(`Server running on http://localhost:3333`);
-  console.log(`Docs avaliable at http://localhost:3333/docs`);
+	console.log(`Server running on http://localhost:3333`);
+	console.log(`Docs avaliable at http://localhost:3333/docs`);
 });
